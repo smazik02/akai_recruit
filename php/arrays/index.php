84bff283771e3e1e5dc6b95d9807149c56fed0a7
smazik02 <s.mazik2002@gmail.com>
2022-10-20 19:27:31 +0200

    Day 1 progress

--- php/arrays/index.php
+++ php/arrays/index.php
@@ -1,15 +1,44 @@
-<?php
-// @author: Marcin Sylka <realmozzar@gmail.com>
-// @date: 28.09.2021
-// @last update: 28.09.2021
-// This is a simple task, which does not require Composer
-// The script will be outputting to a web browser
-// Write a PHP script which will separate every word to key in array
-// example string $str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-// example output: Array ( [0] => Lorem [1] => Ipsum [2] => is [3] => simply [4] => dummy [5] => text [6] => of [7] => the [8] => printing [9] => and [10] => typesetting [11] => industry. )
-// count letters in every word and remove words smaller than 4 letters
-// example output: Array ( [0] => Lorem [1] => Ipsum [3] => simply [4] => dummy [5] => text [8] => printing [10] => typesetting [11] => industry. )
-//
-// Full final output of script:
-// Array ( [0] => Lorem [1] => Ipsum [2] => is [3] => simply [4] => dummy [5] => text [6] => of [7] => the [8] => printing [9] => and [10] => typesetting [11] => industry. )
-// Array ( [0] => Lorem [1] => Ipsum [3] => simply [4] => dummy [5] => text [8] => printing [10] => typesetting [11] => industry. )
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Arrays</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+        <p>Input a sentence <input type="text" name="a"></p>
+        <input type="submit" value="Send" name="send">
+    </form>
+    <?php
+    if (isset($_POST['send'])) {
+        @$sentence = $_POST['a'];
+        $words = explode(" ", $sentence);
+        print_r($words); echo "<br>";
+        $count=count($words);
+        for ($i = 0; $i < $count; $i++) {
+            if (strlen($words[$i]) < 4) {
+                unset($words[$i]);
+            }
+        }
+        print_r($words);
+    }
+    // @author: Marcin Sylka <realmozzar@gmail.com>
+    // @date: 28.09.2021
+    // @last update: 28.09.2021
+    // This is a simple task, which does not require Composer
+    // The script will be outputting to a web browser
+    // Write a PHP script which will separate every word to key in array
+    // example string $str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    // example output: Array ( [0] => Lorem [1] => Ipsum [2] => is [3] => simply [4] => dummy [5] => text [6] => of [7] => the [8] => printing [9] => and [10] => typesetting [11] => industry. )
+    // count letters in every word and remove words smaller than 4 letters
+    // example output: Array ( [0] => Lorem [1] => Ipsum [3] => simply [4] => dummy [5] => text [8] => printing [10] => typesetting [11] => industry. )
+    //
+    // Full final output of script:
+    // Array ( [0] => Lorem [1] => Ipsum [2] => is [3] => simply [4] => dummy [5] => text [6] => of [7] => the [8] => printing [9] => and [10] => typesetting [11] => industry. )
+    // Array ( [0] => Lorem [1] => Ipsum [3] => simply [4] => dummy [5] => text [8] => printing [10] => typesetting [11] => industry. )
+    ?>
+</body>
+
+</html>
--- php/loops/index.php
+++ php/loops/index.php
@@ -1,10 +1,44 @@
-<?php
-// @author: Marcin Lawniczak <marcin@lawniczak.me>
-// @date: 24.10.2017
-// @update: 26.09.2019
-// This is a simple task, which does not require Composer
-// The script will be outputting to a web browser, so use HTML for formatting
-// Write out numbers from 100 to 0, each in a separate line
-// If the number is a multiple of 4, write Fire instead of the number
-// If the number is a multiple of 7, write Boom instead of the number
-// If the number is a multiple of 10, repeat it 10 times in the same line in red
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Loops</title>
+</head>
+
+<body>
+    <?php
+    $end = "";
+    for ($x = 100; $x >= 0; $x--) {
+        $num = "";
+        if ($x == 0) {
+            $num = 0;
+        } else {
+            if ($x % 4 == 0) {
+                $num = "Fire <br>";
+            } else if ($x % 7 == 0) {
+                $num = "Boom <br>";
+            } else {
+                $num = "$x <br>";
+            }
+            if ($x % 10 == 0) {
+                $num = '<span style="color:red">' . $num . '</span>';
+                $num = str_repeat($num, 10);
+            }
+        }
+        $end .= $num;
+    }
+    echo "$end";
+    // @author: Marcin Lawniczak <marcin@lawniczak.me>
+    // @date: 24.10.2017
+    // @update: 26.09.2019
+    // This is a simple task, which does not require Composer
+    // The script will be outputting to a web browser, so use HTML for formatting
+    // Write out numbers from 100 to 0, each in a separate line
+    // If the number is a multiple of 4, write Fire instead of the number
+    // If the number is a multiple of 7, write Boom instead of the number
+    // If the number is a multiple of 10, repeat it 10 times in the same line in red
+    ?>
+</body>
+
+</html>
