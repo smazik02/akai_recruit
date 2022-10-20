d044752ee1feb8931160b715db534b419280fa20
smazik02 <s.mazik2002@gmail.com>
2022-10-20 17:34:37 +0200

    Loaded files

---
+++ python/tasker/src/json/Exporter.py
@@ -0,0 +1,11 @@
+import json
+
+
+class Exporter:
+
+    def __init__(self):
+        pass
+
+    def save_tasks(self, tasks):
+        # TODO zapisz taski do pliku tutaj
+        pass
---
+++ python/tasker/src/json/Importer.py
@@ -0,0 +1,15 @@
+import json
+
+
+class Importer:
+
+    def __init__(self):
+        pass
+
+    def read_tasks(self):
+        # TODO odczytaj plik i zdekoduj treść tutaj
+        pass
+
+    def get_tasks(self):
+        # TODO zwróć zdekodowane taski tutaj
+        pass
