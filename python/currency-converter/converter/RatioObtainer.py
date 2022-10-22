60af74d112451a97e7443da90d3ce84857fae5e0
smazik02 <s.mazik2002@gmail.com>
2022-10-22 11:23:47 +0200

    Work done_2

--- python/currency-converter/converter/RatioObtainer.py
+++ python/currency-converter/converter/RatioObtainer.py
@@ -26,18 +26,11 @@ class RatioObtainer:
         else:
             return False
 
-        # This function checks if given ratio was saved today and if the file with ratios is created at all
-        # should return false when file doesn't exist or if there's no today's exchange rate for given values at all
-        # should return true otherwise
-
     def fetch_ratio(self):
         url = 'https://api.exchangerate.host/convert?from='+self.base+'&to='+self.target
         data = requests.get(url).json()
         if data.get('success')==True:
             self.save_ratio(data.get('result'))
-        # This function calls API for today's exchange ratio
-        # Should ask API for today's exchange ratio with given base and target currency
-        # and call save_ratio method to save it
 
     def save_ratio(self, ratio):
         filename = self.base+'2'+self.target+'.json'
@@ -51,9 +44,6 @@ class RatioObtainer:
         rat=json.dumps(rat, indent=4, separators=(", ", " : "))
         f.write(rat)
         f.close
-        # Should save or update exchange rate for given pair in json file
-        # takes ratio as argument
-        # example file structure is shipped in project's directory, yours can differ (as long as it works)
 
     def get_matched_ratio_value(self):
         filename = self.base+'2'+self.target+'.json'
@@ -62,4 +52,3 @@ class RatioObtainer:
         check=json.load(f)
         rtio=(check.get('ratio'))
         return rtio
-        # Should read file and receive exchange rate for given base and target currency from that file
